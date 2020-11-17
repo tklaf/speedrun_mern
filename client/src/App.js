5f1144a7c19456c9b2388fabdbe38c30acda56e4
Tyler Klafke <Ttylerrk@gmail.com>
2020-11-16 22:40:23 -0800

    started on bootstrap styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
+import './styles/App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import Navbar from "./components/navbar.component"
 import SpeedrunsList from "./components/speedruns-list.component"
 import EditSpeedrun from "./components/edit-speedrun.component"
 import CreateSpeedrun from "./components/create-speedrun.component"
 import CreateUser from "./components/create-user.component"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Router>
+      <div className='container'>
       <Navbar />
       <br/>
       <Route path = '/' exact component = {SpeedrunsList} />
       <Route path = '/edit/:id' component = {EditSpeedrun} />
       <Route path = '/create' component = {CreateSpeedrun} />
       <Route path = '/user' component = {CreateUser} />
+      </div>
     </Router>
   );
 }
--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -1,21 +1,22 @@
 import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import "../styles/navbar.css"
 
 export default class Navbar extends Component {
 
     render(){
         return(
-            <nav>
-            <Link to = "/">Speedrun Tracker</Link>
+            <nav className='navbar navbar-dark'>
+            <Link to = "/" className="navbar-brand">Speedrun Tracker</Link>
             <div>
-                <ul>
-                    <li>
+                <ul className="navbar-nav">
+                    <li className="nav-link">
                         <Link to="/">Speedruns</Link>
                     </li>
-                    <li>
+                    <li className="nav-link">
                         <Link to="/create">Create Speedrun Log</Link>
                     </li>
-                    <li>
+                    <li className="nav-link">
                         <Link to="/user">Create User</Link>
                     </li>
                 </ul>
